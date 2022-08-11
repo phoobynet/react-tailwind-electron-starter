f36993d4157cabea5f4204c0e1cb9e5ea6138135
Glenn <phoobynet@users.noreply.github.com>
2022-08-11 19:33:17 +0100

    Fixed the router

--- renderer/renderer.tsx
+++ renderer/renderer.tsx
@@ -2,12 +2,12 @@ import App from '@/App'
 import '@/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>,
 )
