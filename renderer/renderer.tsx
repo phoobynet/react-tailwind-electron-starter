de203f76dc03b82fa907fcbc1dc5215c7063878e
Glenn <phoobynet@users.noreply.github.com>
2022-08-12 09:46:38 +0100

    Added todo reminder

--- renderer/renderer.tsx
+++ renderer/renderer.tsx
@@ -3,6 +3,8 @@ import '@/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter as Router } from 'react-router-dom'
+
+// TODO: Remove this, but not how shared code is imported inside the renderer folder.
 import { helloFromShared } from '../shared/helloFromShared'
 
 helloFromShared()
